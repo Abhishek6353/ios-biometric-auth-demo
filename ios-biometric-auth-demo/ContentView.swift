5e824a488fc5afa5562657eeec0ab354ee8a38f8
Abhishek Kurmi <itsabhi814@gmail.com>
2025-09-23 17:05:25 +0530

    Initial Commit

---
+++ ios-biometric-auth-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-biometric-auth-demo
+//
+//  Created by Apple on 23/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-biometric-auth-demo/ios_biometric_auth_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_biometric_auth_demoApp.swift
+//  ios-biometric-auth-demo
+//
+//  Created by Apple on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_biometric_auth_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
